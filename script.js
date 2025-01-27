ceb68444f836fc56c95c69d851198438463db01c
dunkdoge <altalanbiju@gmail.com>
2025-01-27 14:38:48 +1100

    added drawable grid

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+var mouseIsDown = false;
+
 function generateGrid(gridSize){
     const container = document.querySelector(".container");
     for (let i = 0; i < gridSize; i++){
@@ -6,11 +8,35 @@ function generateGrid(gridSize){
 
         for (let j = 0; j < gridSize; j++){
             const col = document.createElement("div");
+
+            col.addEventListener("mousedown", function (e) {
+                if (e.type == "mousedown"){
+                    col.style.backgroundColor = "black";
+                    mouseIsDown = true;
+                }
+            });
+
+            col.addEventListener("mouseup", function (e) {
+                if (e.type == "mouseup"){
+                    col.style.backgroundColor = "black";
+                    mouseIsDown = false;
+                }
+            });
+
+            col.addEventListener("mouseenter", function (e) {
+                if (mouseIsDown){
+                    col.style.backgroundColor = "black";
+                }
+            });
+
             col.classList.toggle("col");
             row.appendChild(col);
         }
         container.appendChild(row);
     }
+}
+
+function highlightCell(f) {
 }
 
 window.onload = generateGrid(64);
